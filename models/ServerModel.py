13f9dd2149ed4be653a1218bfa011887bfbbb7c1
Gaston <gaston010.gv@gmail.com>
2023-09-22 13:16:14 -0300

    fix: server list of range

--- models/ServerModel.py
+++ models/ServerModel.py
@@ -58,46 +58,45 @@ class ModelServer:
             raise Exception(e)
 
     @classmethod
-    def check_server(cls, nombre):
+    def check_server(cls, nombre_servidor):
         """Comprueba si existe un servidor con el nombre pasado por parametro"""
         conn = Conexion()
-        try:
-            sql = 'SELECT nombre_servidor FROM servidor WHERE nombre_servidor = %s'
-            value = nombre
-            conn.execute(sql, (value,))
-            server = conn.fetchall()
-            if server[0][0] == nombre:
-                return True
-            else:
-                return False
-        except Exception as e:
-            raise Exception(e)
+
+        sql = 'SELECT nombre_servidor FROM servidor WHERE nombre_servidor = %s'
+        conn.execute(sql, (nombre_servidor,))
+        server = conn.fetchall()
+        if not server:
+            return False
+        else:
+            return True
 
     @classmethod
     def add_server(cls, nombre, descripcion, autor):
         """Añade un servidor a la base de datos, si el mismo ya existe devuelve un mensaje de error"""
         if cls.check_server(nombre):
             return {'message': 'Server already exists'}, 400
-        else:
-            conn = Conexion()
-            try:
-                sql = 'INSERT INTO servidor (nombre_servidor, descripcion, autor_id) VALUES (%s, %s, %s)'
-                conn.execute(sql, (nombre, descripcion, autor))
-                conn.commit()
-                if conn.rowcount() > 0:
-                    data_response = {
-                        'message': 'Server created successfully',
-                        'New Server info:': cls.get_by_last_add(),
-                    }
-                    return data_response, 201
-                else:
-                    data_response = {
-                        'message': 'Server not created a error'
-                    }
-                    return data_response, 400
 
-            except Exception as e:
-                raise Exception(e)
+        conn = Conexion()
+        try:
+            sql = 'INSERT INTO servidor (nombre_servidor, descripcion, autor_id) VALUES (%s, %s, %s)'
+            conn.execute(sql, (nombre, descripcion, autor))
+            conn.commit()
+            if conn.rowcount() > 0:
+                data_response = {
+                    'message': 'Server created successfully',
+                    "New server info": {
+                        "id_servidor": cls.get_by_last_add()
+                    }
+                }
+                return data_response, 201
+            else:
+                data_response = {
+                    'message': 'Server not created a error'
+                }
+                return data_response, 400
+
+        except Exception as e:
+            raise Exception(e)
 
     @classmethod
     def server_update(cls, nombre_servidor, descripcion, id_server):
