e1b658c610cef49d80d02de82faf0fbbd98f8311
Gaston <gaston010.gv@gmail.com>
2023-09-29 00:38:51 -0300

    Fix: login user

--- models/LoginModel.py
+++ models/LoginModel.py
@@ -1,8 +1,8 @@
+import re
+
 from exceptions.ExceptionsHandler import MissingData, UserNotFound
 from models.entity.User import UserLogin
 from utils.Conexion import Conexion
-
-import re
 
 
 class LoginUser:
@@ -14,10 +14,11 @@ class LoginUser:
         if not email or not password:
             raise MissingData()
 
-        sql = """SELECT id_usuario, nombre, email, nick , avatar, nick, password
+        sql = """SELECT id_usuario, nombre, email, nick, avatar, nick, password
                 FROM usuario WHERE email = %s AND password = %s"""
         con.execute(sql, (email, password))
         user = con.fetchall()
+
         if not user:
             raise UserNotFound()
         else:
@@ -25,6 +26,10 @@ class LoginUser:
             for usser in user:
                 item = UserLogin(usser[0], usser[1], usser[2], usser[3], usser[4], usser[5])
                 login_user.append(item.to_json_login())
+
+            if not login_user:
+                raise UserNotFound()
+
             return login_user, 200
 
     @classmethod
